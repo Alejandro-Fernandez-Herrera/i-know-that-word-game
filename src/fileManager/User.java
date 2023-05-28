1c5ac56bcdc9b29e450afdf25e8b04e98e4f4796
Alejandro-Fernandez-Herrera <alejandro.fernandez.herrera@correounivalle.edu.co>
2023-05-28 17:21:45 -0500

    revision

---
+++ src/fileManager/User.java
@@ -0,0 +1,25 @@
+package fileManager;
+
+
+
+public class User {
+/**
+ * actions performed by the users, they may log in, create a new account or exit the program
+ */
+
+    public User {
+        FileManager fileManager = new FileManager();
+        userList = fileManager.reader("users.txt");
+        levelList = fileManager.reader("levels.txt");
+        position = 0;
+        user = "";
+        level = 0;
+
+    }
+
+
+
+
+
+
+}
